eba43b30dedd7fce997829187701df33d64f781d
LVRMWT17 <lera.luckaya77@gmail.com>
2025-01-30 19:55:08 +0300

    Update server.js

--- server.js
+++ server.js
@@ -63,7 +63,26 @@ app.post('/login', (req, res) => {
             return res.json({ message: 'Successfully'})
     });
 });
-})
+});
+app.post('/register', (req, res) => {
+    const { email, password, firstName, lastName, registrationTime } = req.body;
+    const checkQuery = 'SELECT * FROM users2 WHERE email = ?';
+    db.execute(checkQuery, [email], (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error. Try again.' });
+        }
+        if (results.length > 0) {
+            return res.status(400).json({ message: 'A user with such email already exists.' });
+        }
+    const query = 'INSERT INTO users2 (email, password, first_name, last_name, last_visit) VALUES (?, ?, ?, ?, ?)';
+    db.execute(query, [email, password, firstName, lastName, registrationTime], (err, results) => {
+        if (err) {
+            return res.status(500).json({ message: 'An error during registration. Try again.' });
+        }
+        return res.json({message: 'Successfully'})
+    });
+});
+});
 
 app.get('/public/table.html', (_req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'table.html'));
